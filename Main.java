a770748f9e945efeb3139794000471c56f0c0d4c
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-11-03 21:35:11 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package days26;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int nilai = 50;
+        int angka = 20;
+        
+        
+        System.out.println(nilai>angka);
+        System.out.println(nilai%angka);
+        System.out.println(nilai<angka);
+        System.out.println(nilai*angka);
+    }
+    
+}
